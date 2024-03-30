05590dd5a45cd4cef317f1135ee20360789f835f
jubjub727 <matthewbrockgosnell@gmail.com>
2024-03-30 14:27:01 +1300

    Created client class

---
+++ tmps-client/TMPSClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+
+namespace LSWTSS.OMP;
+
+public class TMPSClient : IDisposable
+{
+    public void Dispose()
+    {
+
+    }
+
+    ~TMPSClient()
+    {
+        Dispose();
+    }
+}

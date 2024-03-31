050c3cb473302a975e652254c1d51dfec40a59d3
jubjub727 <matthewbrockgosnell@gmail.com>
2024-03-31 19:10:21 +1300

    Added Scene Dumper

---
+++ SceneDumper/Node.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using LSWTSS.OMP.Game.Api;
+
+
+namespace LSWTSS.OMP;
+public class Node
+{
+    [JsonIgnore]
+    public static apiEntity.Handle rootNode;
+
+    [JsonIgnore]
+    public Node Parent;
+
+    public string Label { get; set; }
+    public List<Node> Children { get; set; }
+
+    [JsonIgnore]
+    public apiComponent.Handle Component = (apiComponent.Handle)nint.Zero;
+    [JsonIgnore]
+    public apiEntity.Handle Entity = (apiEntity.Handle)nint.Zero;
+
+    [JsonIgnore]
+    public BigInteger Guid;
+
+    [JsonIgnore]
+    public string Name;
+
+    [JsonIgnore]
+    private int IterationCount = 0;
+
+    public Node(Node parent)
+    {
+        Children = new List<Node>();
+        Parent = parent;
+    }
+
+    public Node(apiEntity.Handle entity)
+    {
+        Children = new List<Node>();
+        Entity = entity;
+        Component = (apiComponent.Handle)(nint)entity;
+        rootNode = entity;
+        Label = Entity.GetLabel();
+    }
+
+    private bool ComponentIsEntity(apiComponent.Handle component)
+    {
+        var apiEntityVtablePtr = Marshal.ReadIntPtr(component.Ptr);
+        return (apiEntityVtablePtr == NativeFunc.GetPtr(0x41E8B90));
+    }
+
+    public bool FindChildComponent(string[] componentNames)
+    {
+        foreach (string componentName in componentNames)
+        {
+            if (IsEntity())
+            {
+                apiComponent.Handle component = Entity.FindComponentByTypeName(componentName);
+
+                if (component != nint.Zero)
+                {
+                    Node child = CreateChild(component);
+                    Children.Add(child);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static BigInteger GetGuid(apiComponent.Handle component)
+    {
+        var guidPart1 = Marshal.ReadInt64(component.Ptr + 0x46);
+        var guidPart2 = Marshal.ReadInt64(component.Ptr + 0x4E);
+
+        return new BigInteger(guidPart2) << 64 | new BigInteger(guidPart1);
+    }
+
+    private Node FindChild(BigInteger guid)
+    {
+        foreach (Node child in Children)
+        {
+            if (child.Guid == guid)
+            {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    private Node CreateChild(apiComponent.Handle component)
+    {
+        Node child = new Node(this);
+
+        if (ComponentIsEntity(component))
+        {
+            child.Entity = (apiEntity.Handle)(nint)component;
+            child.Component = component;
+
+            child.Name = child.Entity.GetName();
+        }
+        else
+        {
+            child.Component = component;
+        }
+
+        child.Label = child.Component.GetLabel();
+
+        child.Guid = GetGuid(child.Component);
+
+        //Console.WriteLine("Found Child with label - " + child.Label);
+
+        return child;
+    }
+
+    private void IterateForwards(Node node)
+    {
+        IterationCount++;
+        if (IterationCount > 1000)
+        {
+            return;
+        }
+        apiComponent.Handle nextComponent = node.Component.NextSibling();
+        if (nextComponent != nint.Zero)
+        {
+            Node child = FindChild(GetGuid(nextComponent));
+
+            if (child == null)
+            {
+                child = CreateChild(nextComponent);
+                Children.Add(child);
+            }
+
+            IterateForwards(child);
+        }
+    }
+
+    private void IterateBackwards(Node node)
+    {
+        IterationCount++;
+        if (IterationCount > 1000)
+        {
+            return;
+        }
+        apiComponent.Handle prevComponent = node.Component.PrevSibling();
+        if (prevComponent != nint.Zero)
+        {
+            Node child = FindChild(GetGuid(prevComponent));
+
+            if (child == null)
+            {
+                child = CreateChild(prevComponent);
+                Children.Add(child);
+            }
+
+            IterateBackwards(child);
+        }
+    }
+
+    public void FindAllSiblings()
+    {
+        IterationCount = 0;
+        IterateForwards(this);
+        IterationCount = 0;
+        IterateBackwards(this);
+    }
+
+    public void FindAllChildren()
+    {
+        if (Children.Count < 1)
+        {
+            return;
+        }
+        IterationCount = 0;
+        IterateForwards((Node)Children[0]);
+        IterationCount = 0;
+        IterateBackwards((Node)Children[0]);
+    }
+
+    public bool IsEntity()
+    {
+        return Entity != nint.Zero;
+    }
+}
---
+++ SceneDumper/TestMod.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using LSWTSS.OMP.Game.Api;
+using System.Text.Json;
+
+
+namespace LSWTSS.OMP;
+
+public class TestMod : IDisposable
+{
+    [DllImport("user32.dll")]
+    static extern short GetAsyncKeyState(int vKey);
+
+    private const string componentsPath = @"D:\lswtss-omp-export-latest\apiComponentsDumped.txt";
+
+    private string[] componentNames;
+
+    private Node rootNode;
+
+    private nttUniverse.Handle nttUniverse;
+
+    private PlayerControlSystem.Handle playerControlSystemHandle;
+
+    NativeFuncHook<DoubleJumpContext.OnEnterMethod.Delegate>? doubleJumpContextOnEnterMethodHook;
+
+    void DoCoolStuff(DoubleJumpContext.Handle handle)
+    {
+        var apiEntityHandle = handle.GetParent();
+        if (apiEntityHandle == nint.Zero)
+        {
+            throw new Exception("Cannot get apiEntity");
+        }
+
+        nttUniverse = apiEntityHandle.GetUniverse();
+
+        if (nttUniverse == nint.Zero)
+        {
+            throw new Exception("Cannot get nttUniverse");
+        }
+
+        playerControlSystemHandle = PlayerControlSystem.GetFromGlobalFunc.Execute(nttUniverse);
+
+        if (playerControlSystemHandle == nint.Zero)
+        {
+            throw new Exception("Cannot get PlayerControlSystem");
+        }
+
+        apiEntity.Handle localPlayer = playerControlSystemHandle.GetPlayerEntityForPlayerIdx(0);
+
+        if (localPlayer == nint.Zero)
+        {
+            throw new Exception("Cannot get LocalPlayer");
+        }
+
+        ApiWorld.Handle apiWorld = localPlayer.GetWorld();
+
+        if (apiWorld == nint.Zero)
+        {
+            throw new Exception("Cannot get ApiWorld");
+        }
+
+        rootNode = new Node(apiWorld.GetSceneGraphRoot());
+
+        if (rootNode.Entity == nint.Zero)
+        {
+            throw new Exception("Cannot get RootNode");
+        }
+
+        /*apiEntity.Handle parent = localPlayer.GetParent();
+
+        if (parent == nint.Zero)
+        {
+            throw new Exception("Cannot get PartyMembers");
+        }
+
+        rootNode = new Node(parent.GetParent());
+
+        if (rootNode.Entity == nint.Zero)
+        {
+            throw new Exception("Cannot get RootNode");
+        }*/
+
+
+
+        RecurseChildren(rootNode);
+
+        File.WriteAllText(@"nodegraph.json", JsonSerializer.Serialize(rootNode));
+    }
+
+    private void RecurseChildren(Node node)
+    {
+        if (node.IsEntity())
+        {
+            if (node.FindChildComponent(componentNames))
+            {
+                node.FindAllChildren();
+
+                foreach (Node childNode in node.Children)
+                {
+                    RecurseChildren(childNode);
+                }
+            }
+        }
+    }
+
+    public TestMod()
+    {
+        componentNames = File.ReadAllLines(componentsPath);
+
+        doubleJumpContextOnEnterMethodHook = new(
+            DoubleJumpContext.OnEnterMethod.Ptr,
+            (DoubleJumpContext.Handle handle, ArgsEnter.Handle param0) =>
+            {
+                try
+                {
+                    DoCoolStuff(handle);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("EXCEPTION: " + e);
+                }
+
+                doubleJumpContextOnEnterMethodHook!.GetTrampoline()(handle, param0);
+            }
+        );
+
+        doubleJumpContextOnEnterMethodHook.Enable();
+
+        /*nttUniverse = GetUniverseGlobalFunc.Execute();
+
+        if (nttUniverse == nint.Zero)
+        {
+            throw new Exception("Cannot get nttUniverse");
+        }*/
+
+
+    }
+
+    public void OnUpdate()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        if (doubleJumpContextOnEnterMethodHook != null)
+        {
+            doubleJumpContextOnEnterMethodHook.Dispose();
+            doubleJumpContextOnEnterMethodHook = null;
+        }
+    }
+
+    ~TestMod()
+    {
+        Dispose();
+    }
+}

1b5ab7baeec06e020bd9782e25002b54bd070851
jubjub727 <matthewbrockgosnell@gmail.com>
2024-05-27 05:34:58 +1200

    Interp changes

--- networking/Utils.cs
+++ networking/Utils.cs
@@ -18,7 +18,7 @@
         public const int DefaultTimeout = 3000;
 
         public const int PacketDepth = 24;
-        public const int PacketOffset = 16;
+        public const int PacketOffset = 20;
 
         public static bool IsBadFloat(float x)
         {
